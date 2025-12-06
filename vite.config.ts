4deb17b5db00a2bcfa570f34fecedba318c4e856
rlundev <ronaldwin79@hotmail.com>
2025-12-06 05:59:30 -0400

    run on own ip

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: true, // Permite acceso desde la IP de tu máquina
+    port: 8000,
   },
 })
